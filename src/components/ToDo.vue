ee10c47952db23891c5b428eaee068cad49efd47
Nguyễn Bá Hà <74853522+Nguyenbaha@users.noreply.github.com>
2022-04-20 14:05:46 +0700

    first commit

---
+++ src/components/ToDo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="ToDo">
+    <h1>Luyen tap Vue</h1>
+    <div class="container">
+      <div id="tasks">
+        <h2> Nhiem Vu Moi </h2>
+          <ol class="list-group">
+            <li v-for = "(task,index) in tasks" :key="index" class="list-group-item">
+              {{task.body}}
+            </li>
+          </ol>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  el: "#tasks",
+  data: function() {
+    return {
+      tasks: [
+              {body:'Di Lam 0',completed: false},
+              {body:'Di Lam 1',completed: false},
+              {body:'Di Lam 2',completed: false},
+              {body:'Di Lam 3',completed: false},
+              ]
+      
+      }
+  }
+};
+</script>
